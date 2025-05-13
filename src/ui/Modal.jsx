52e825033e705229d6887e702b6cc2bd8c77a4e0
Saidul147 <sidul.islam.146@gmail.com>
2025-05-13 21:54:30 +0600

    form added on dialog

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,22 +1,66 @@
 import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
-export default function Modal({isOpen,setIsOpen}) {
-//   let [isOpen, setIsOpen] = useState(false)
+export default function Modal({ isOpen, setIsOpen }) {
+  //   let [isOpen, setIsOpen] = useState(false)
+
+  const { register, reset, handleSubmit } = useForm()
+
+  const onSubmitForm = (data) => {
+    console.log(data)
+    setIsOpen(false)
+    handleCancel()
+  }
+  const handleCancel = () => {
+    reset()
+  }
 
   return (
     <>
       {/* <button onClick={() => setIsOpen(true)}>Open dialog</button> */}
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <Description>This will permanently deactivate your account</Description>
-            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
-            <div className="flex gap-4">
-              <button className='bg-yellow-400 px-3 py-2 rounded-lg' onClick={() => setIsOpen(false)}>Cancel</button>
-              <button className='bg-gray-400 px-3 py-2 rounded-lg'  onClick={() => setIsOpen(false)}>Deactivate</button>
-            </div>
+          <DialogPanel className="md:w-[40vw] h-[90vh] overflow-y-auto rounded-lg shadow-md space-y-4 border bg-white p-12">
+            <DialogTitle className="font-bold">Fill up the Info</DialogTitle>
+            {/* <Description>This will permanently deactivate your account</Description> */}
+            <form onSubmit={handleSubmit(onSubmitForm)}>
+              <div className='space-y-4'>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="title">Title</label>
+                  <input type="text" id="title" {...register("title")} className='rounded-md' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="descript">Description</label>
+                  <input type="text" id="descript" {...register("descript")} className='rounded-md' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="deadline">Deadline</label>
+                  <input type="date" id="deadline" {...register("deadline")} className='rounded-md' />
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="assign">Assign To</label>
+                  <select id="assign" {...register("assign")} defaultValue="select" >
+                    {/* <option value="" disabled defaultValue="Select">Selct</option> */}
+                    <option value="Rahim">Rahim</option>
+                    <option value="Karim">Karim</option>
+                    <option value="Tarin">Tarin</option>
+                  </select>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor="priority">Priority</label>
+                  <select id="priority" {...register("priority")} >
+                    <option value="Low">Low</option>
+                    <option value="High">High</option>
+                  </select>
+                </div>
+              </div>
+              <div className='flex gap-3 justify-end'>
+
+              <button onClick={handleCancel} className='px-[10px] flex py-[6px] bg-red-500 border hover:bg-red-600 border-green-200 rounded-lg mt-3 text-white'  >cancel</button>
+              <button className='px-[10px]  py-[6px] bg-green-500 hover:bg-green-600 border border-green-200 rounded-lg mt-3 text-white' type='submit'>submit</button>
+              </div>
+            </form>
           </DialogPanel>
         </div>
       </Dialog>
